51f9c1a1e82904ffadd52b6eb013ba7d8bf0a6c6
kimjunseoo <zz0905k@naver.com>
2023-03-09 07:02:25 +0900

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var 이름 = 'Kim';
+var 나이 = 22;
+var bornWhere = "Seoul";
+var favSingerAndSong = { name: "Day6", songname: "Zombie" };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true
+};
---
+++ index.ts
@@ -0,0 +1,11 @@
+const 이름:string = 'Kim';
+const 나이:number = 22;
+const bornWhere:string = "Seoul";
+
+const favSingerAndSong:{ name: string, songname: string} = { name: "Day6", songname: "Zombie"};
+
+let project:{ member: string[], days: number, started: boolean } = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
+}
