5d6867faecae117678d8f51ad9e718fb7ead8a9a
kimjunseoo <zz0905k@naver.com>
2023-03-11 16:44:39 +0900

    20230311 Narrowing&Assertion

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var 이름 = 'Kim';
-var 나이 = 22;
-var bornWhere = "Seoul";
-var favSingerAndSong = { name: "Day6", songname: "Zombie" };
-var project = {
-    member: ['kim', 'park'],
-    days: 30,
-    started: true
-};
+//Narrowing
+function sampleFunc(x) {
+    if (typeof x === 'number') {
+        return x + 1;
+    }
+    else {
+        return x + '1';
+    }
+}
+;
+function sampleFunc2(x) {
+    var array = [];
+    if (typeof x === 'number') {
+        array[0] = x;
+    }
+    ;
+}
+;
+//Assertion
+function sampleFunc3(x) {
+    var array = [];
+    array[0] = x;
+}
+;
+//asserion 언제 쓰냐 => 비상용 / 디버깅용
+sampleFunc(123);
--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
-const 이름:string = 'Kim';
-const 나이:number = 22;
-const bornWhere:string = "Seoul";
+//Narrowing
+function sampleFunc(x: number | string){
+    if( typeof x === 'number'){
+        return x + 1;
+    }else {
+        return x + '1';
+    }
+};
 
-const favSingerAndSong:{ name: string, songname: string} = { name: "Day6", songname: "Zombie"};
+function sampleFunc2(x: number | string){
+    let array: number[] = [];
+    if( typeof x === 'number'){
+        array[0] = x;
+    };
+};
 
-let project:{ member: string[], days: number, started: boolean } = {
-    member : ['kim', 'park'],
-    days : 30,
-    started : true,
-}
+//Assertion
+function sampleFunc3(x: number | string){
+    let array: number[] = [];
+    array[0] = x as number;
+
+};
+
+//asserion 언제 쓰냐 => 비상용 / 디버깅용
+
+
+sampleFunc(123);
